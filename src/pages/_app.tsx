3d21ca49af1b5f539022bed20f6edb786e722685
opazupa <opazupa>
2024-01-24 19:01:48 +0200

    style: new icon set

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function CrackerApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <title>Cracker app</title>
+        <title>Cracker app🍪</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
@@ -37,7 +37,7 @@ export default function CrackerApp({ Component, pageProps }: AppProps) {
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <link rel="apple-touch-icon" href="icons/apple-icon.png"></link>
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <SSRProvider>
